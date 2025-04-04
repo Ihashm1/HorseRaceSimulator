4a36cf10004b5decc2095d75a04942b611f1b4fe
Ihashm1 <ishrathashmi73@outlook.com>
2025-04-04 21:11:47 +0100

    Added fields and constructor to Horse class

---
+++ Part1/Horse.java
@@ -0,0 +1,82 @@
+
+/**
+ * Write a description of class Horse here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Horse
+{
+    //Fields of class Horse
+    private String name;          
+    private char symbol;          
+    private int distanceTravelled; 
+    private boolean hasFallen;    
+    private double confidence;
+
+    //Constructor of class Horse
+    /**
+     * Constructor for objects of class Horse
+     */
+    public Horse(char horseSymbol, String horseName, double horseConfidence) {
+        this.symbol = horseSymbol;
+        this.name = horseName;
+        this.distanceTravelled = 0;
+        this.hasFallen = false;
+        setConfidence(horseConfidence); // Ensure valid confidence level
+    }
+    
+    
+    
+    //Other methods of class Horse
+    public void fall()
+    {
+        
+    }
+    
+    public double getConfidence()
+    {
+        
+    }
+    
+    public int getDistanceTravelled()
+    {
+        
+    }
+    
+    public String getName()
+    {
+        
+    }
+    
+    public char getSymbol()
+    {
+        
+    }
+    
+    public void goBackToStart()
+    {
+        
+    }
+    
+    public boolean hasFallen()
+    {
+        
+    }
+
+    public void moveForward()
+    {
+        
+    }
+
+    public void setConfidence(double newConfidence)
+    {
+        
+    }
+    
+    public void setSymbol(char newSymbol)
+    {
+        
+    }
+    
+}
