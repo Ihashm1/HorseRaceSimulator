c3908bc69832ac4fe1a287916e8df8033aa02cba
Ihashm1 <ishrathashmi73@outlook.com>
2025-05-03 22:53:03 +0100

    Updated HorseConfig to include coatcolour

--- Part2/HorseConfig.java
+++ Part2/HorseConfig.java
@@ -3,11 +3,16 @@ public class HorseConfig {
     public String symbol;
     public int speedBonus;
     public double confidenceBonus;
+    public String coatColor;
 
-    public HorseConfig(String name, String symbol, int speedBonus, double confidenceBonus) {
-        this.name = name;
-        this.symbol = symbol;
-        this.speedBonus = speedBonus;
-        this.confidenceBonus = confidenceBonus;
+    public HorseConfig(String name, String symbol, int speedBonus, double confidenceBonus, String coatColor) {
+    this.name = name;
+    this.symbol = symbol;
+    this.speedBonus = speedBonus;
+    this.confidenceBonus = confidenceBonus;
+    this.coatColor = coatColor;
     }
 }
+
+
+
